b676ba1dc4ee89e05351e5ce72bdc5b1f8def073
mlukicdesign <mlukicdesign@gmail.com>
2023-08-14 20:39:47 +0800

    changed routing

--- dev-portfolio/src/components/Home.js
+++ dev-portfolio/src/components/Home.js
@@ -1,10 +1,8 @@
-import Navigation from './Navigation';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HeroSection from './HeroSection';
 import ProfileSection from './ProfileSection';
 import Capabilities from './Capabilities';
 import SelectedWorks from './Works';
-
 
 
 // Consolidated components into home page to allow correct routing
