e605c70f434280ab6f0fb0c860cfd9b63d65aa9c
mlukicdesign <mlukicdesign@gmail.com>
2023-08-14 21:09:21 +0800

    reverted routes to browser router

--- dev-portfolio/src/App.js
+++ dev-portfolio/src/App.js
@@ -3,7 +3,7 @@ import Navigation from './components/Navigation';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Contact from './components/Contact';
 import Project1 from './components/Project1';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import PageTransition from './components/PageTransition';
 
@@ -15,12 +15,10 @@ function App() {
     <div className="App">
       <Navigation/>
       <PageTransition>
-    <HashRouter>
       <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path='/project1' element={<Project1 />}></Route>
       </Routes>
-    </HashRouter>
       </PageTransition>
       <Contact />
     </div>
--- dev-portfolio/src/components/Navigation.js
+++ dev-portfolio/src/components/Navigation.js
@@ -19,14 +19,14 @@ function Navigation() {
     return (
       <Navbar expand="lg" className="navContainer" fixed='top'>
         <Container className='navBar'>
-          <Navbar.Brand href="#" style={styles.navLinks}>MACKENZIE LUKIC</Navbar.Brand>
+          <Navbar.Brand href="/" style={styles.navLinks}>MACKENZIE LUKIC</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav>
-              <Nav.Link href="/profile" style={styles.navLinks}>ABOUT</Nav.Link>
-              <Nav.Link href="/capabilities" style={styles.navLinks}>PROCESS</Nav.Link>
-              <Nav.Link href="/works" style={styles.navLinks}>WORK</Nav.Link>
-              <Nav.Link className='buttonLight' href="/contact">GET IN TOUCH</Nav.Link>
+              <Nav.Link href="#profile" style={styles.navLinks}>ABOUT</Nav.Link>
+              <Nav.Link href="#capabilities" style={styles.navLinks}>PROCESS</Nav.Link>
+              <Nav.Link href="#works" style={styles.navLinks}>WORK</Nav.Link>
+              <Nav.Link className='buttonLight' href="#contact">GET IN TOUCH</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
